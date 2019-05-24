8597a81b46119b90418b8e2ef7216c4422cc69cf
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2019-05-24 15:32:46 -0600

    Update navigation.js

--- navigation/files/navigation.js
+++ navigation/files/navigation.js
@@ -302,10 +302,11 @@ else {pagNation()}
 		
 	$("h2.nav-title").wrap("<summary />")
 		};
-			$.getScript("https://wet-boew.github.io/themes-dist/GCWeb/wet-boew/js/polyfills/details.js");
+			
 		}
 	if($('#nav-pull').length > 0) {	
 $('#nav-pull').on( "wb-contentupdated", function( event, data ){
 mobileCollapse()  });}	
 else {mobileCollapse()}
+		$.getScript("https://wet-boew.github.io/themes-dist/GCWeb/wet-boew/js/polyfills/details.js");
 	})( jQuery, document, wb );
